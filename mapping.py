55e7eaf655e89cb102b3c0702e10135714edf97f
KwakMinSeon <118345489+KwakMinSeon@users.noreply.github.com>
2026-01-30 14:56:30 +0900

    remove dir

--- mapping.py
+++ mapping.py
@@ -2,25 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# .env 로드
-load_dotenv()
-# 레포루트(Code_it-Basic-Project)
-REPO_ROOT = Path(__file__).resolve().parent 
-# .env를 정확히 지정해서 로드
-load_dotenv(REPO_ROOT / ".env")
-
-# .env에서 데이터 루트 가져오기
-DATA_ROOT = os.getenv("LOG_FILE_PATH")
-if DATA_ROOT is None:
-    raise ValueError("LOG_FILE_PATH가 .env에 설정되어 있지 않습니다.")
-
-DATA_ROOT = Path(DATA_ROOT)
-print('data root :', DATA_ROOT)
-
-# 경로설정 ---------------------------------------------
-TRAIN_IMG_DIR = DATA_ROOT / "train_images"
-TEST_IMG_DIR = DATA_ROOT / "test_images"
-ANNOTATION_DIR = DATA_ROOT / "train_annotations"
+from Dataset.Dataset_load import TRAIN_IMG_DIR, TEST_IMG_DIR, ANNOTATION_DIR, CACHE_DIR, DATA_ROOT
 
 print(TRAIN_IMG_DIR)
 print(len(os.listdir(TRAIN_IMG_DIR)))
