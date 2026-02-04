d4c637359ba7d4efb3abbf25a727c716032ec52b
KwakMinSeon <118345489+KwakMinSeon@users.noreply.github.com>
2026-02-04 17:12:27 +0900

    adding main

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from basic-project!")
-
+from train_test_split import multilabel_train_val_split
+from Dataset.Dataset_load import TRAIN_IMG_DIR, TEST_IMG_DIR, ANNOTATION_DIR, CACHE_DIR, DATA_ROOT
+import json
+from pathlib import Path
 
 if __name__ == "__main__":
-    main()
+
+    gt_cache_path = CACHE_DIR / "targets_by_filename.json"
+    with open(gt_cache_path, "r", encoding="utf-8") as f:
+        targets_by_filename = json.load(f)
+
+    train_fnames, val_fnames = multilabel_train_val_split(
+        targets_by_filename,
+        val_ratio=0.33,
+        seed=42
+    )
+    print("train:", len(train_fnames))
+    print("val  :", len(val_fnames))
